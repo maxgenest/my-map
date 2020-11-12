a691456f50bfa69936c43a1440e3515a801245d9
Maxime <maxime@mbp-de-maxime.lan>
2020-11-12 12:18:30 +0100

    new map

---
+++ beach tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="beach tilesheet" tilewidth="32" tileheight="32" spacing="4" tilecount="11400" columns="60">
+ <image source="../../Downloads/beach tilesheet.png" width="2160" height="6840"/>
+</tileset>
