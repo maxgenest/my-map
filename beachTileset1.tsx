0ac9f9269c708867914284c77eb8524caf155a18
Maxime <maxime@mbp-de-maxime.lan>
2020-11-13 10:57:23 +0100

    new tileset

---
+++ beachTileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="beachTileset1" tilewidth="32" tileheight="32" spacing="4" tilecount="11400" columns="60">
+ <image source="beach tilesheet.png" width="2160" height="6840"/>
+</tileset>
